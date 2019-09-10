90d64ec9a0ee3c8a24e671262b29b48cdfe580c7
A V A <ava@As-MacBook-Pro.fritz.box>
2019-09-10 11:57:35 +0200

    Updates

---
+++ precache-manifest.52c11411f3a41cfc781eab867ff58915.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62117ae9cc47f586d34fcaf8672cc993",
+    "url": "/what4DO/index.html"
+  },
+  {
+    "revision": "42de5f830755b68699d8",
+    "url": "/what4DO/static/css/2.7d6f7eac.chunk.css"
+  },
+  {
+    "revision": "331094cc9c761ff2dd09",
+    "url": "/what4DO/static/css/main.c8998647.chunk.css"
+  },
+  {
+    "revision": "42de5f830755b68699d8",
+    "url": "/what4DO/static/js/2.1d7f44a6.chunk.js"
+  },
+  {
+    "revision": "331094cc9c761ff2dd09",
+    "url": "/what4DO/static/js/main.97aa233e.chunk.js"
+  },
+  {
+    "revision": "a96da711ae839e3b87bf",
+    "url": "/what4DO/static/js/runtime~main.35e70dc1.js"
+  },
+  {
+    "revision": "17268478cc8e805bf02cc73349f0531f",
+    "url": "/what4DO/static/media/Antic-Regular.17268478.ttf"
+  },
+  {
+    "revision": "b184964cf07f8cff0a407b8cee7b1b4d",
+    "url": "/what4DO/static/media/Ketosag.b184964c.otf"
+  },
+  {
+    "revision": "16f46eba33197c9b17c1963448d91a2f",
+    "url": "/what4DO/static/media/LOGO.16f46eba.png"
+  },
+  {
+    "revision": "843e6c09e8e8052681fdf5da7a1df321",
+    "url": "/what4DO/static/media/ethnocentric rg.843e6c09.ttf"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/what4DO/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/what4DO/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/what4DO/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/what4DO/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/what4DO/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "77c78300fdd101b315934c7104382cd9",
+    "url": "/what4DO/static/media/gratisinBerlin_9_9.77c78300.csv"
+  }
+]);
